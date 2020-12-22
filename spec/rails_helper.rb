cf09c83ae40ca63af15a674857b1bc7c615e7ddc
mahesvara5829 <76080506+mahesvara5829@users.noreply.github.com>
2020-12-23 07:59:27 +0900

    結合テストの実装

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,6 +31,8 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+
 I18n.locale = "en"
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
@@ -40,6 +42,8 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+
+  config.include SignInSupport
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
